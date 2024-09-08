9850f878af1d664d19998193c46c06e8d115870f
marcovittoriozacca <zaccamarco01@gmail.com>
2024-09-08 12:01:36 +0200

    feat: post service functions declaration - not finished

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { PostService } from './post.service';
 
 @Controller('post')
 export class PostController {
+  constructor(private postService: PostService) {}
   @Get('/')
   async getAllPosts() {
     return;
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { Post } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(private prisma: PrismaService) {}
+
+  async getAllPosts(): Promise<Post[]> {
+    //this is only an example of the returned value - not finished
+    //@ts-ignore
+    return [{ id: 1 }, { id: 2 }, { id: 3 }];
+  }
+
+  async getPostById(): Promise<Post> {
+    //this is only an example of the returned value - not finished
+    //@ts-ignore
+    return [{ id: 1 }];
+  }
+
+  async createNewPost(): Promise<Post> {
+    //this is only an example of the returned value - not finished
+    //@ts-ignore
+    return [{ new_post: true }];
+  }
+
+  async updatePost(): Promise<Post> {
+    //this is only an example of the returned value - not finished
+    //@ts-ignore
+    return [{ updated_post: true }];
+  }
+
+  async deletePost(): Promise<{ success: boolean }> {
+    //this is only an example of the returned value - not finished
+    //@ts-ignore
+    return { success: true };
+  }
+}
